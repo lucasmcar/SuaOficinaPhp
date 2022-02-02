d5e3ee69da9894b92ee2dae124f197a1d4b14e6b
Devpoa Software <suporte.devpoa@gmail.com>
2022-02-02 00:33:46 -0300

    SSO-V1 - Adicionado gitignore, css e tela de login

--- index.php
+++ index.php
@@ -7,5 +7,6 @@ $titulo= "Sistema Sua Oficina";
 
 $smarty->assign("titulo", $titulo);
 
-$smarty->display('main-header/header.tpl');
-
+$smarty->display('default/header.tpl');
+$smarty->display('default/footer.tpl');
+$smarty->display('login/login.tpl');
