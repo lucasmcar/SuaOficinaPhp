21677a52d69601c1ad093650648c4d59418440b3
Lucas John <lucas.ti.john@gmail.com>
2022-02-08 21:23:08 -0300

    Desenvolvido método insert para OrcamentoDao.php

--- core/dao/oficina/OrcamentoDao.php
+++ core/dao/oficina/OrcamentoDao.php
@@ -1,12 +1,29 @@
 <?php
 
-namespace core\dao\oficina;
+namespace core\dao\orcamento;
 
-use core\vo\oficina\OficinaVo;
+use core\vo\oficina\OrcamentoVo;
 use core\connection\Connection;
 use PDOException;
 
 class OrcamentoDao
 {
+    public function insert(OrcamentoVo $vo)
+    {
+        try{
+            $sql = "INSERT INTO orcamento (codorcamento, dtpreventrega, dtpreventrega, cdoficina) VALUES (:codorcamento, :dtpreventrega, :dsorcamento, :cdoficina)";
+            $con = Connection::getConnection();
+            $stmt = $con->prepare($sql);
+            $stmt->bindValue(':codorcamento', $vo->__get('codorcamento'));
+            $stmt->bindValue(':dtpreventrega', $vo->__get('dtpreventrega'));
+            $stmt->bindValue(':dtpreventrega', $vo->__get('dtpreventrega'));
+            $stmt->bindValue(':cdoficina', $vo->__get('cdoficina'));
+            $result = $stmt->execute(); 
+            if($result){
+                return true;
+            }
+        } catch(PDOException $ex) {
 
+        }
+    }
 }
