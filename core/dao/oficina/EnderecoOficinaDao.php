d7edeb912bc6e90afedb020eb2bf8a556b699319
Lucas John <lucas.ti.john@gmail.com>
2022-02-08 21:40:39 -0300

    Desenvolvido método insert para EnderecoOficinaDao.php

--- core/dao/oficina/EnderecoOficinaDao.php
+++ core/dao/oficina/EnderecoOficinaDao.php
@@ -2,11 +2,34 @@
 
 namespace core\dao\oficina;
 
-use core\vo\oficina\OficinaVo;
+use core\vo\oficina\EnderecoOficinaVo;
 use core\connection\Connection;
 use PDOException;
 
 class EnderecoOficinaDao
 {
-
+    public function insert(EnderecoOficinaVo $vo)
+    {
+        try{
+            $sql = "INSERT INTO endereco_oficina (logradouro, bairro, nrlogradouro, tipo, cdoficina, cdcidade) VALUES (:logradouro, :bairro, :nrlogradouro, :tipo, :cdoficina, :cdcidade)";
+            $con = Connection::getConnection();
+            $stmt = $con->prepare($sql);
+            $stmt->bindValue(':logradouro', $vo->__get('logradouro'));
+            $stmt->bindValue(':bairro', $vo->__get('bairro'));
+            $stmt->bindValue(':nrlogradouro', $vo->__get('nrlogradouro'));
+            $stmt->bindValue(':tipo', $vo->__get('tipo'));
+            $stmt->bindValue(':cdoficina', $vo->__get('cdoficina'));
+            $stmt->bindValue(':cdcidade', $vo->__get('cdcidade'));
+            $result = $stmt->execute(); 
+            if($result){
+                return true;
+            }
+        } catch(PDOException $ex) {
+            $this->arrayError = [
+                'error_msg' => $ex->getMessage(),
+                'error_code' => $ex->getCode()
+            ];
+            return json_encode($this->arrayError);
+        }
+    }
 }
